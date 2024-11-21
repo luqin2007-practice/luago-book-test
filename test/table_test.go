db0dceee67487e1f4ad42e60ab05dbec3a3e261b
luqin2007 <lq2007lq@hotmail.com>
2024-11-22 02:34:20 +0800

    ch7 table

--- test/table_test.go
+++ test/table_test.go
@@ -10,6 +10,46 @@ import (
 	"testing"
 )
 
+/*
+main <.\table.lua:0,0> (14 instructions at 0000000000be9160)
+0+ params, 6 slots, 1 upvalue, 2 locals, 9 constants, 0 functions
+
+	1       [1]     NEWTABLE        0 3 0
+	2       [1]     LOADK           1 -1    ; "a"
+	3       [1]     LOADK           2 -2    ; "b"
+	4       [1]     LOADK           3 -3    ; "c"
+	5       [1]     SETLIST         0 3 1   ; 1
+	6       [2]     SETTABLE        0 -4 -5 ; 2 "B"
+	7       [3]     SETTABLE        0 -6 -7 ; "foo" "Bar"
+	8       [5]     GETTABLE        1 0 -8  ; 3
+	9       [5]     GETTABLE        2 0 -4  ; 2
+	10      [5]     GETTABLE        3 0 -9  ; 1
+	11      [5]     GETTABLE        4 0 -6  ; "foo"
+	12      [5]     LEN             5 0
+	13      [5]     CONCAT          1 1 5
+	14      [5]     RETURN          0 1
+
+constants (9) for 0000000000be9160:
+
+	1       "a"
+	2       "b"
+	3       "c"
+	4       2
+	5       "B"
+	6       "foo"
+	7       "Bar"
+	8       3
+	9       1
+
+locals (2) for 0000000000be9160:
+
+	0       t       6       15
+	1       s       14      15
+
+upvalues (1) for 0000000000be9160:
+
+	0       _ENV    1       0
+*/
 func TestTable(t *testing.T) {
 	data, err := os.ReadFile("table.out")
 	if err != nil {
